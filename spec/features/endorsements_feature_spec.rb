c4aed4be82da1074e518aa9e6b3ddba6b1964fe9
chn.challenger <chn.challenger@gmail.com>
2015-10-17 11:26:00 +0100

    endorsements working and tests

---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'endorsing reviews' do
+
+  scenario 'a user can endorse a review, which updates the review endorsement count' do
+    kfc = Restaurant.create(name: 'KFC')
+    kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
+    user = create(:user)
+    sign_in(user)
+    visit '/restaurants'
+    click_link 'Endorse Review' #are we endorsing restaurants or the review of the restaurants?
+    expect(page).to have_content('1 endorsement')
+  end
+
+end
